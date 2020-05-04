10ea5808b425bacbcec319674e3d3e57dce8ae3c
Kevin Lai <kevinlai76@gmail.com>
2020-05-03 23:41:14 -0400

    add examples for arrowfunctions

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,21 +1,21 @@
-// // 'original'
-// function example0(name) {
-//     return `hi ${name}`
-// }
+// 'original'
+function example0(name) {
+    return `hi ${name}`
+}
 
-// // 'default'
-// let example1 = (name) => {return `hi ${name}`}
+// 'default'
+let example1 = (name) => {return `hi ${name}`}
 
-// // if one parameter parentheses are optional
-// let example2 = name => {return `hi ${name}`}
+// if one parameter parentheses are optional
+let example2 = name => {return `hi ${name}`}
 
-// // if the only statement is a return we can remove the curly brackets and the return
-// let example3 = name => `hi ${name}`
+// if the only statement is a return we can remove the curly brackets and the return
+let example3 = name => `hi ${name}`
 
-// console.log(example0('dave'))
-// console.log(example1('dave'))
-// console.log(example2('dave'))
-// console.log(example3('dave'))
+console.log(example0('dave'))
+console.log(example1('dave'))
+console.log(example2('dave'))
+console.log(example3('dave'))
 
 class Person {
     constructor(name) {
